4028e4f545d69484242ecc79af82143ab4b1ebf0
Ayoub Sousali <sousali.x@gmail.com>
2022-09-22 21:39:17 +0100

    added cities json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import GalleryContent from './components/cards/galleryContent';
 import Layout from './components/common/layout';
 
 function App() {
   return (
     <Layout>
       <div className="my-8">
-        <GalleryContent />
+        <p>next salat</p>
       </div>
     </Layout>
   );
