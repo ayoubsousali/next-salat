f3f1ade5f9ba36d4d49c3ea7b3dc380121d192b2
Ayoub Sousali <sousali.x@gmail.com>
2022-09-23 15:56:23 +0100

    added new arabic font

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -2,7 +2,7 @@
 
 export default function DefaultLayout({ children }) {
   return (
-    <div className="lg:flex lg:justify-center lg:items-center bg-[#000] text-white">
+    <div className="lg:flex lg:justify-center lg:items-center bg-[#000] text-white font-serif">
       <div className="w-screen overflow-hidden max-w-[2560px]">
         <div className="min-h-screen relative text-white flex flex-col">
           <div className="flex-grow">{children}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {},
+      fontFamily: {
+        serif: 'Noto Naskh Arabic, serif',
+      },
       colors: {},
     },
   },
