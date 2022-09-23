a0a773c2e226eec7d1bf2a07057092e44dc2f606
Ayoub Sousali <sousali.x@gmail.com>
2022-09-23 17:12:44 +0100

    added date of the day

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,13 @@ import SelectCities from './SelectCities';
 export default function Main() {
   const [prayers, setPrayers] = useState({});
 
+  const date = new Date();
+  const dateFormatted = new Intl.DateTimeFormat('fr-MA').format(date);
+
   return (
     <main>
       <h1 className="text-center text-3xl p-4">أوقات الصلاة بالمغرب</h1>
+      <h2 className="text-center text-xl p-2">ليوم {dateFormatted}</h2>
       <SelectCities setPrayers={setPrayers} />
       <Prayer name="الفجر" time={prayers.Fajr} />
       <Prayer name="الشروق" time={prayers.Chorouq} />
