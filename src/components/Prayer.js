b227d108d3280faa593226457709ba1d8c2cc490
Ayoub Sousali <sousali.x@gmail.com>
2022-09-24 13:37:54 +0100

    added bold font in prayer time & justify between

--- src/components/Prayer.js
+++ src/components/Prayer.js
@@ -8,10 +8,10 @@ export default function Prayer({ name, time, isNext }) {
         isNext
           ? 'dark:border-red-500 border-border-red-500 border-b-4 dark:text-red-500 text-red-500'
           : 'dark:border-light border-dark border-b-2 dark:text-light text-dark'
-      } flex justify-items-center justify-evenly	p-2 m-2 max-w-[600px]`}
+      } flex justify-items-center justify-between	p-2 m-2 max-w-[600px]`}
     >
       <span>{arabicName(name)}</span>
-      <span>{time}</span>
+      <span className="font-bold">{time}</span>
     </div>
   );
 }
