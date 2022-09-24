ddd1b10cdb3065192b5c0a5e57fdcbe9f3a59aac
Ayoub Sousali <sousali.x@gmail.com>
2022-09-24 14:04:40 +0100

    fixed typo in border css class

--- src/components/Prayer.js
+++ src/components/Prayer.js
@@ -6,12 +6,12 @@ export default function Prayer({ name, time, isNext }) {
     <div
       className={`${
         isNext
-          ? 'dark:border-red-500 border-border-red-500 border-b-4 dark:text-red-500 text-red-500'
+          ? 'dark:border-red-500 border-red-500 border-b-4 dark:text-red-500 text-red-500'
           : 'dark:border-light border-dark border-b-2 dark:text-light text-dark'
       } flex justify-items-center justify-between	p-2 m-2 max-w-[600px]`}
     >
       <span>{arabicName(name)}</span>
-      <span className="font-bold">{time}</span>
+      <span className="font-semibold">{time}</span>
     </div>
   );
 }
