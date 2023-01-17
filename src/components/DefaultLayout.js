61c1ebb6efa83fc83f813035cdb2cec82e8e8207
Ayoub Sousali <sousali.x@gmail.com>
2023-01-17 14:42:26 +0100

    added link to view code on github

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 
+import Footer from './Footer';
+
 export default function DefaultLayout({ children }) {
   return (
     <div className="lg:flex lg:justify-center lg:items-center dark:bg-dark bg-light dark:text-light text-dark font-serif">
       <div className="w-screen overflow-hidden max-w-[2560px]">
         <div className="min-h-screen relative flex flex-col">
           <div className="flex-grow">{children}</div>
+          <Footer />
         </div>
       </div>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 export default function Footer() {
   return (
-    <footer className="p-2 max-w-screen bg-nt_black">
-      <div className="flex justify-between" />
+    <footer className="p-2 max-w-screen">
+      <div className="flex justify-center">
+        <a
+          href="https://github.com/ayoubsousali/next-salat"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/github.svg"
+            width="20"
+            className="m-1.5"
+            title="View code on Github"
+            alt="Github"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
